3461db5b6f379492e954a38e853b1066aa9f2b50
Samay's Macbook Pro <samay@Macbook-Pro>
2026-01-30 01:21:32 -0700

    Modified lsp.lua to add keybindings

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -52,3 +52,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.lsp.enable("pyright")
   end,
 })
+
+-- LSP Keybindings
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = { buffer = args.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  end,
+})
