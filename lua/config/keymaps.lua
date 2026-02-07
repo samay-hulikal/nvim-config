bc187fbb6bac05bcf837126868c02cd3b68e0fbf
Samay's Mac mini <samay@Samays-Mac-mini>
2026-02-07 14:34:13 -0700

    Added snippets for tex and keymaps to jump between tab stops

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,9 +5,18 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
 
 -- Half-page scrolling with centered cursor
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz0')
+vim.keymap.set('n', '<C-u>', '<C-u>zz0')
 
 -- Keep search results centered
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Jumping between tab stops for snippets (works for .tex, .py, anything)
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
+  require("luasnip").jump(1)
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+  require("luasnip").jump(-1)
+end, { silent = true })
