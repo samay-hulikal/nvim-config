469d937fd4f18195aa8bbe1a685fd666716a7e7a
Samay's Mac mini <samay@Samays-Mac-mini>
2026-03-23 11:39:21 -0600

    Modified lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,6 +9,14 @@ return {
         section_separators = "",
         component_separators = "",
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
     })
   end,
 }
