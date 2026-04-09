3a96118c98dc52c63992ee7685ff842659a5fc0c
Samay's Macbook Pro <samay@Macbook-Pro>
2026-04-09 13:38:57 -0600

    Changed the autocmds.lua file to work with telescope instead

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,20 +2,37 @@
 vim.api.nvim_create_autocmd("BufNewFile", {
   pattern = "*.tex",
   callback = function()
-    local template_dir = vim.fn.expand("~/.config/nvim/templates/")
-    local files = vim.fn.glob(template_dir .. "*.tex", false, true)
+    vim.defer_fn(function()
+      local template_dir = vim.fn.expand("~/.config/nvim/templates/")
+      local files = vim.fn.glob(template_dir .. "*.tex", false, true)
+      if #files == 0 then return end
 
-    if #files == 0 then return end
+      local pickers = require("telescope.pickers")
+      local finders = require("telescope.finders")
+      local conf = require("telescope.config").values
+      local actions = require("telescope.actions")
+      local action_state = require("telescope.actions.state")
 
-    local names = {}
-    for _, f in ipairs(files) do
-      table.insert(names, vim.fn.fnamemodify(f, ":t:r"))
-    end
-
-    vim.ui.select(names, { prompt = "Choose template (Esc to skip):" }, function(choice)
-      if choice then
-        vim.cmd("0r " .. template_dir .. choice .. ".tex")
+      local names = {}
+      for _, f in ipairs(files) do
+        table.insert(names, vim.fn.fnamemodify(f, ":t:r"))
       end
+
+      pickers.new({}, {
+        prompt_title = "Choose Template",
+        finder = finders.new_table({ results = names }),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            local selection = action_state.get_selected_entry()
+            actions.close(prompt_bufnr)
+            if selection then
+              vim.cmd("0r " .. template_dir .. selection[1] .. ".tex")
+            end
+          end)
+          return true
+        end,
+      }):find()
     end, 50)
   end,
 })
