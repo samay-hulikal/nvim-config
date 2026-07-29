8d9d50d3246229855d75c264ecdbb04e66e08eeb
Samay Fedora <samay@fedora>
2026-07-29 16:35:30 -0600

    Changes to the vim server to prevent the vimtex errors when I have multiple instaces of nvim running

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -10,7 +10,8 @@ return {
     -- else
       -- vim.g.vimtex_view_method = "okular"
     else
-      vim.fn.serverstart("/tmp/nvim")
+      -- vim.fn.serverstart("/tmp/nvim")
+      pcall(vim.fn.serverstart, "/tmp/nvim")
       vim.g.vimtex_view_method = "general"
       vim.g.vimtex_view_general_viewer = "okular"
       vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
